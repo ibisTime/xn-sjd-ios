e6828f2cf4e36ede0668b40d1340a14171a62eb2
杰 <792574770@qq.com>
2019-01-25 10:02:15 +0800

    no message

---
+++ Time Island/我的/View/Address/AddressPickView.h
@@ -0,0 +1,39 @@
+//
+//  AddressPickView.h
+//  AddressView
+//
+//  Created by ding on 2017/12/29.
+//  Copyright © 2017年 ding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define kWidth ([UIScreen mainScreen].bounds.size.width)
+#define kHeight ([UIScreen mainScreen].bounds.size.height)
+
+
+typedef void(^determineBtnActionBlock)(NSString *shengId, NSString *shiId, NSString *xianId, NSString *shengName, NSString *shiName, NSString *xianName,NSString *postCode);
+
+@interface AddressPickView : UIView
+
+@property (nonatomic, copy) determineBtnActionBlock determineBtnBlock;
+
+- (instancetype)initWithFrame:(CGRect)frame;
+- (void)show;
+- (void)dismiss;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@end
