d13884516be19865a57eee1df84472d099d29487
杰 <792574770@qq.com>
2019-01-25 13:43:45 +0800

    no message

--- Time Island/我的/Controller/Setting/GetAddVC.h
+++ Time Island/我的/Controller/Setting/GetAddVC.h
@@ -11,6 +11,21 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface GetAddVC : TLBaseVC<UITextFieldDelegate>
+@property (nonatomic,strong) TLTextField * Name;
+@property (nonatomic,strong) NSString * NameString;
+@property (nonatomic,strong) TLTextField * Phone;
+@property (nonatomic,strong) NSString * PhoneString;
+@property (nonatomic,strong) TLTextField * Address;
+@property (nonatomic,strong) NSString * AddressString;
+@property (nonatomic,strong) TLTextField * DoorNum;
+@property (nonatomic,strong) NSString * DoorNumString;
+//@property (nonatomic,strong) AddressPickView * pickerView;
+@property (nonatomic,strong) NSString * sheng;
+@property (nonatomic,strong) NSString * shi;
+@property (nonatomic,strong) NSString * qu;
+@property (nonatomic,assign) int state;
+@property (nonatomic,strong) NSString * AddressCode;
+
 
 @end
 
