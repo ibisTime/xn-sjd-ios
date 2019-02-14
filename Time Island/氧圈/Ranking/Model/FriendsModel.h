6b0c45cf8e0b91ccc89bea224ebec2bfdd374e80
杰 <792574770@qq.com>
2019-02-14 13:49:41 +0800

    no message

---
+++ Time Island/氧圈/Ranking/Model/FriendsModel.h
@@ -0,0 +1,24 @@
+//
+//  FriendsModel.h
+//  Time Island
+//
+//  Created by 梅敏杰 on 2019/2/14.
+//  Copyright © 2019年 ChengLian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FriendsModel : NSObject
+@property (nonatomic,strong) NSString * certificateCount;
+@property (nonatomic,strong) NSString * code;
+@property (nonatomic,strong) NSString * createDatetime;
+@property (nonatomic,strong) NSString * rowNo;
+@property (nonatomic,strong) NSString * status;
+@property (nonatomic,strong) NSString * toUser;
+@property (nonatomic,strong) NSString * type;
+@property (nonatomic,strong) NSString * userId;
+@end
+
+NS_ASSUME_NONNULL_END
