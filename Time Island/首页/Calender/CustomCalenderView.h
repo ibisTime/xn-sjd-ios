ed4da6899d8550f5cf3e6b0b7281ff7e1163cf96
郑勤宝 <13506537519@163.com>
2019-02-27 16:19:00 +0800

    1

---
+++ Time Island/首页/Calender/CustomCalenderView.h
@@ -0,0 +1,16 @@
+//
+//  CustomCalenderView.h
+//  Time Island
+//
+//  Created by 郑勤宝 on 2019/2/27.
+//  Copyright © 2019 ChengLian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomCalenderView : UIView
+
+@property (nonatomic , strong)NSArray *selectArray;
+@property (nonatomic , strong)NSArray *allDaysArray;
+@property (nonatomic , assign)NSInteger dayLength;
+@end
